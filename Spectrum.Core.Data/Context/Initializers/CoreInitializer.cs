de4691c024996e1889755cc02c7aa57d9baec05e
Patrick Welch <patrick.welch@spectrumoperational.com>
2015-11-17 17:37:27 -0700

    Added organization types to the core initializer.

--- Spectrum.Core.Data/Context/Initializers/CoreInitializer.cs
+++ Spectrum.Core.Data/Context/Initializers/CoreInitializer.cs
@@ -15,17 +15,64 @@ namespace Spectrum.Core.Data.Context.Initializers
             {
                 new User()
                 {
-                    UserName = "superusers@spectrumoperational.com",
+                    UserName = "superuser",
                     Email = "superusers@spectrumoperational.com",
-                    PasswordHash = hasher.HashPassword("password")
+                    PasswordHash = hasher.HashPassword("p@ssw0rd")
                 },
                 new User()
                 {
-                    UserName = "patrick.welch@spectrumoperational.com",
+                    UserName = "Patrick Welch",
                     Email = "patrick.welch@spectrumoperational.com",
-                    PasswordHash = hasher.HashPassword("password")
+                    PasswordHash = hasher.HashPassword("p@ssw0rd")
                 }
             }.ForEach(u => context.Users.Add(u));
+
+            new List<OrganizationType>
+            {
+                new OrganizationType()
+                {
+                    Id = 1,
+                    Name = "Local Government",
+                    Description = "Municipality, city, town or township, bourough that has corporate status and local government"
+                },
+                new OrganizationType()
+                {
+                    Id = 2,
+                    Name = "County Government",
+                    Description = "County governments"
+                },
+                new OrganizationType()
+                {
+                    Id = 3,
+                    Name = "State Government",
+                    Description = "State governments"
+                },
+                new OrganizationType()
+                {
+                    Id = 4,
+                    Name = "Federal Government",
+                    Description = "Federal government"
+                },
+                new OrganizationType()
+                {
+                    Id = 5,
+                    Name = "Non-government Organization",
+                    Description = "Non-government organization (example: Red Cross)"
+                },
+                new OrganizationType()
+                {
+                    Id = 6,
+                    Name = "Hospital",
+                    Description = "Hospitals"
+                },
+                new OrganizationType()
+                {
+                    Id = 7,
+                    Name = "Private",
+                    Description = "Private companies and organizations"
+                },
+            }.ForEach(t => context.OrganizationTypes.Add(t));
+
 
             context.SaveChanges();
             base.Seed(context);
