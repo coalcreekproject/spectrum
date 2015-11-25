a50872f1ce0ad712a1b7b573ce781c79d00b7878
Patrick Welch <patrick.welch@spectrumoperational.com>
2015-11-25 12:27:51 -0700

    Added a Logic Utility to start helping with user functions.  I expect to re-work this.

--- Spectrum.Logic/Identity/UserUtility.cs
+++ Spectrum.Logic/Identity/UserUtility.cs
@@ -1,26 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Spectrum.Core.Data.Caching;
 using Spectrum.Core.Data.Caching.Extensions;
+using Spectrum.Core.Data.Models;
 using Spectrum.Logic.Models;
+using StackExchange.Redis;
 
 namespace Spectrum.Logic.Identity
 {
-    class UserUtility
+    public static class UserUtility
     {
-        public int GetLoggedInUserId()
+        public static int GetLoggedInUserId(IPrincipal user)
         {
-            //TODO: Get User out of cache.
-            //var cache = RedisCache.Connection.GetDatabase();
-            //var userModel = cache.StringGet("user:" + userModel.Id, userModel);
+            var id = user.Identity.GetUserId();
+            return Convert.ToInt32(id);
+        }
 
+        public static UserModel GetUserFromCache(IPrincipal user)
+        {
+            var id = user.Identity.GetUserId();
 
-            return 1;
+            var cache = RedisCache.Connection.GetDatabase();
+            var userModel = cache.Get<UserModel>("user:" + id);
+
+            return userModel;
         }
     }
 }
