ba7a2c06693aa05492e0aedae3090081fa721e3e
Patrick Welch <patrick.welch@spectrumoperational.com>
2015-12-01 00:04:48 -0700

    Update to the database.  Roles are not working well at all.  EF graph problems.

--- Spectrum.Core.Database/Tables/UserRole.sql
+++ Spectrum.Core.Database/Tables/UserRole.sql
@@ -5,13 +5,5 @@
 
     CONSTRAINT [PK_UserRole_UserID_RoleID] PRIMARY KEY CLUSTERED ([UserId] ASC, [RoleId] ASC),
     CONSTRAINT [FK_UserRole_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id]) ON DELETE CASCADE,
-    CONSTRAINT [FK_UserRole_UserRole] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Role] ([Id]) ON DELETE CASCADE, 
+    CONSTRAINT [FK_UserRole_Role] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Role] ([Id]) ON DELETE CASCADE, 
 );
-
-GO
-CREATE NONCLUSTERED INDEX [IX_UserRole_UserID]
-    ON [dbo].[UserRole] ([UserId] ASC);
-
-GO
-CREATE NONCLUSTERED INDEX [IX_UserRole_RoleID]
-    ON [dbo].[UserRole] ([RoleId] ASC);
