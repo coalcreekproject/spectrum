79fa1d4f42f39b01bb3c83fa92c802e090259037
Patrick Welch <patrick.welch@spectrumoperational.com>
2015-12-11 00:31:59 -0700

    Initial laying down of the list elements.

---
+++ Spectrum.Web/Areas/Eoc/Controllers/ChecklistController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spectrum.Web.Areas.Eoc.Controllers
+{
+    public class ChecklistController : Controller
+    {
+        // GET: Eoc/Checklist
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Template(string template)
+        {
+            switch (template.ToLower())
+            {
+                case "checklistindex":
+                    return PartialView("~/Areas/Eoc/Views/Checklist/Partials/ChecklistIndex.cshtml");
+                default:
+                    throw new ApplicationException("Unknown Template");
+            }
+        }
+    }
+}
---
+++ Spectrum.Web/Scripts/spectrum-eoc/checklists.js
@@ -0,0 +1,11 @@
+﻿angular
+    .module('app')
+    .controller('CheckListsController', CheckListsController);
+
+function userRoleParameters() {
+    this.userId = null;
+    this.organizationId = null;
+};
+
+function UserRolesModalController($scope, $modalInstance, userRoleFactory, user) {
+}
