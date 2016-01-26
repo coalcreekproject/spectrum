a0ad1edbc470ef88e2ed97d5d2f54861716cf8e8
Patrick Welch <patrick.welch@spectrumoperational.com>
2016-01-25 17:03:41 -0700

    Added some more position elements.

---
+++ Spectrum.Web/Models/UserPositionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Spectrum.Web.Models
+{
+    public class UserPositionViewModel
+    {
+        public int UserId { get; set; }
+        public int PositionId { get; set; }
+
+        public virtual PositionViewModel Position { get; set; }
+        public virtual UserViewModel User { get; set; }
+    }
+}
