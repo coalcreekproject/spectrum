0afcf95cd84a57f492792454e1e2bcb8cae9cb5d
Patrick Welch <patrick.welch@spectrumoperational.com>
2016-02-19 09:26:22 -0700

    Added viewmodel for focus.

---
+++ Spectrum.Web/Models/IdentityFocusViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spectrum.Web.Models
+{
+    public class IdentityFocusViewModel
+    {
+        public int UserId { get; set; }
+        public int OrganizationId { get; set; }
+        public int RoleId { get; set; }
+        public int PositionId { get; set; }
+
+        public List<OrganizationViewModel> OrganizationViewModels { get; set; }
+        public List<RoleViewModel> RoleViewModels { get; set; }
+        public List<PositionViewModel> PositionViewModels { get; set; }
+    }
+}
