5c4dcbb25ca13f903646c1bf95d4df41b0d389c5
Patrick Welch <patrick.welch@spectrumoperational.com>
2016-03-06 22:38:05 -0700

    Why this was left out I have no idea.

---
+++ Spectrum.Web/Controllers/Web/CommunityController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Spectrum.Web.Controllers.Web
+{
+    public class CommunityController : Controller
+    {
+        // GET: Community
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Template(string template)
+        {
+            switch (template.ToLower())
+            {
+                case "communityindex":
+                    return PartialView("~/Views/Community/Partials/CommunityIndex.cshtml");
+                default:
+                    throw new ApplicationException("Unknown Template");
+            }
+        }
+    }
+}
