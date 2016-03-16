b17ce345255c1274f521757652bca37c79dab0dc
Sean Smith <smithyhammer@gmail.com>
2016-03-15 21:41:36 -0600

    - First check-in for Position Log screens. - Setup pages and routing and some basic functionality.

---
+++ Spectrum.Web/Areas/Eoc/Controllers/Web/PositionLogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Spectrum.Web.Areas.Eoc.Controllers.Web
+{
+    public class PositionLogController : Controller
+    {
+        // GET: Eoc/PositionLog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Template(string template)
+        {
+            switch (template.ToLower())
+            {
+                case "positionlogindex":
+                    return
+                        PartialView("~/Areas/Eoc/Views/PositionLog/Partials/PositionLogIndex.cshtml");
+                case "positionlogmanagement":
+                    return
+                        PartialView("~/Areas/Eoc/Views/PositionLog/Partials/PositionLogManagement.cshtml");
+                default:
+                    throw new ApplicationException("Unknown Template");
+            }
+        }
+    }
+}
---
+++ Spectrum.Web/Scripts/app/eoc/position-log-mgmt-ctrl.js
@@ -0,0 +1,60 @@
+﻿(function() {
+    angular
+        .module("app.eoc")
+        .controller("PositionLogMgmtCtrl", PositionLogMgmtCtrl)
+        .config(config);
+
+    config.$inject = ["$locationProvider", "$stateProvider", "$urlRouterProvider", "$compileProvider"];
+
+    function config($locationProvider, $stateProvider, $urlRouterProvider, $compileProvider) {
+
+        // Optimize load start with remove binding information inside the DOM element
+        $compileProvider.debugInfoEnabled(true);
+
+        $stateProvider
+            .state("posmgmt", {
+                url: "/manage/:incidentId",
+                templateUrl: "/Eoc/Templates/PositionLog/PositionLogManagement",
+                controller: "PositionLogMgmtCtrl",
+                controllerAs: "plm"
+            });
+    }
+
+    PositionLogMgmtCtrl.$inject = ["$scope", "incidentData"];
+
+    function PositionLogMgmtCtrl($scope, incidentData) {
+
+        var vm = this;
+
+        vm.incidentName = "Mile High Island";
+        vm.name = "Willie";
+        vm.logs = [
+            {
+                "id": 1,
+                "logName": "Updates on Patient Symptoms",
+                "logDate": new Date(),
+                "log": "It's been 14 days since I killed me a man."
+            },
+            {
+                "id": 2,
+                "logName": "Casualties Starting to Arrive",
+                "logDate": new Date(),
+                "log": "I once saw a man with no legs."
+            }
+        ];
+        vm.search = "";
+        vm.clear = clear;
+
+        function clear() {
+            vm.search = "";
+        }
+
+        function activate() {
+            
+        }
+
+        // Activate the page
+        activate();
+    }
+
+})();
