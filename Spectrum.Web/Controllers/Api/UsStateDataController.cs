793248859eecd3a98417247a35247ca4c9ed5f15
Patrick Welch <patrick.welch@spectrumoperational.com>
2016-03-26 01:00:49 -0600

    Added WebAPI controller to serve up state data and 3 state and county json data files.

---
+++ Spectrum.Web/Controllers/Api/UsStateDataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace Spectrum.Web.Controllers.Api
+{
+    public class UsStateDataController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            var json = File.ReadAllText(HttpContext.Current.Server.MapPath(@"~/App_Data/states_titlecase.json"));
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
